1c411125dbf20e5a8778dd9f4c3e023b3d93843b
hasibmuhammad <hasibmu99@gmail.com>
2023-11-30 22:34:34 +0600

    json middleware added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
@@ -39,6 +40,7 @@ app.get("/users", (req, res) => {
 });
 
 app.post("/users", (req, res) => {
+  console.log("Post is hitting");
   console.log(req.body);
 });
 
