9a343898c15f818a874edd29cc4076c2f830e78e
hasibmuhammad <hasibmu99@gmail.com>
2023-11-30 14:09:21 +0600

    users served

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
+
 const port = process.env.PORT || 5000;
+
+const users = [
+  {
+    id: 1,
+    name: "shuvo",
+    age: 30,
+  },
+  {
+    id: 2,
+    name: "Hasib",
+    age: 20,
+  },
+  {
+    id: 3,
+    name: "joy",
+    age: 40,
+  },
+  {
+    id: 4,
+    name: "sohel",
+    age: 18,
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("Welcome to backend!");
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
 
 app.listen(port, (req, res) => {
   console.log(`Server is runnig on port: ${port}`);
